b708eacc985cbf28a4323a2a4e4beb81b4720433
汪心禾 <wangxinhe06@gmail.com>
2020-02-25 22:00:48 +0800

    base64

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
+const io = require('socket.io')();
+const crypto = require('crypto');
 
 const app = express();
 
@@ -8,5 +10,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/javascripts', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
+
+io.on('connection', (socket) => {
+  socket.on('submit', (url) => {
+    const base64 = crypto.createHmac('md5', url).digest('base64');
+    socket.emit('shortened', base64);
+  });
+});
+
+app.set('socket.io', io);
 
 module.exports = app;
