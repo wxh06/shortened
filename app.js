5b575d5f0351fd801cda1c4835ec357e199bb055
汪心禾 <wangxinhe06@gmail.com>
2020-02-26 15:18:04 +0800

    fs

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const logger = require('morgan');
 const io = require('socket.io')();
 const crypto = require('crypto');
+const fs = require('fs');
 
 const app = express();
 
@@ -13,13 +14,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/javascripts', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
 
+
+let urls;
+try {
+  urls = JSON.parse(fs.readFileSync('urls.json'));
+} catch (e) {
+  urls = {};
+}
+
 io.on('connection', (socket) => {
   socket.on('submit', (url) => {
-    const base64 = crypto.createHmac('md5', url).digest('base64');
+    let base64 = urls[url];
+    if (base64) {
+      base64 = urls.url;
+    } else {
+      base64 = crypto.createHmac('md5', url).digest('base64');
+      urls[url] = base64;
+    }
+    fs.promises.writeFile('urls.json', JSON.stringify(urls));
     socket.emit('shortened', base64);
   });
 });
 
 app.set('socket.io', io);
+
 
 module.exports = app;
